ee74c2fa545cd74d507d58300afdebc5a76ff0fe
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-05-27 20:55:17 +0530

    add ReshapeMatrix_566.java

--- src/ReshapeMatrix_566.java
+++ src/ReshapeMatrix_566.java
@@ -1,17 +1,27 @@
+import java.util.Arrays;
+
 public class ReshapeMatrix_566 {
     public static void main(String[] args) {
+        int[][] mat = {{1, 2, 3, 4}};
+
+        int r = 2;
+        int c = 2;
+
         int a = 0;
         int b = 0;
-        int length = 2;
-        int[][] fin = new int[2][2];
-        for(int i=0; i<fin.length; i++) {
-            for(int j=0; j<fin[i].length; j++, b++) {
-                if(b > length-1) {
+        int[][] fin = new int[r][c];
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++, b++) {
+                if(b > mat[0].length-1 && mat.length > 1) {
                     b = 0;
                     a++;
                 }
-                System.out.println(a + " " + b);
+                fin[i][j] = mat[a][b];
             }
+        }
+
+        for(int[] arr: fin) {
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
