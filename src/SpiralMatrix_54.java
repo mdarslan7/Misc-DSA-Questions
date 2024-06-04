1b58ea93c63516476b3249d36ef6956e57bd31bf
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-06-05 03:15:16 +0530

    add correct solution for Spiral Matrix

--- src/SpiralMatrix_54.java
+++ src/SpiralMatrix_54.java
@@ -1,9 +1,52 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SpiralMatrix_54 {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
 
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+            };
 
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while(rowStart <= rowEnd && colStart <= colEnd) {
+            //upper boundary
+            for(int i = colStart; i <= colEnd; i++) {
+                list.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+
+            //rightmost boundary
+            for(int i = rowStart; i <= rowEnd; i++) {
+                list.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            //bottom boundary
+            if(rowStart <= rowEnd) {
+                for(int i = colEnd; i >= colStart; i--) {
+                    list.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            //leftmost boundary
+            if(colStart <= colEnd) {
+                for(int i = rowEnd; i >= rowStart; i--) {
+                    list.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
+        }
+
+        System.out.println(list);
     }
 }
